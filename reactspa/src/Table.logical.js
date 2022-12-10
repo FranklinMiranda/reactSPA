510be77df34002c2210d4d815d6737d879b6b35d
FranklinMiranda <fdm61344@gmail.com>
2022-12-10 13:58:13 -0500

    Cm

--- reactspa/src/Table.logical.js
+++ reactspa/src/Table.logical.js
@@ -1,8 +1,32 @@
 import React from "react";
 
-const Table = () => {
+// Simple Functional Component for a Table Header
+const TableHeader = () => {
+    return (
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+    )
+  }
+
+// Simple Functional Component for a Table Body
+const TableBody = () => {
     return (
         <tbody></tbody>
     )
 }
 
+// React Component for a Table
+class Table extends React.Component {
+    render() {
+        return (
+            <table>
+                <TableHeader/>
+                <TableBody/>
+            </table>
+        )
+    }
+}
