e959d71285be88b092cc81e468b89bfe49069af1
FranklinMiranda <fdm61344@gmail.com>
2022-12-10 14:11:24 -0500

    Cm

--- reactspa/src/App.js
+++ reactspa/src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Table from "./Table.logical"
 
+// The characters variable is being passed through to the Table Component using a attribute characterData
 class App extends React.Component {
     render() {
+        const characters = [
+            {name: "Franklin", job: "Software Engineer"}, {name: "Anna", job: "Mechanical Engineer"}
+        ]
+
         return (
             <div className="App">
                 <h1>React Single Page Application</h1>
                 <div className="container-Table">
-                    <Table/>
+                    <Table characterData={characters}/>
                 </div>
             </div>
         )
