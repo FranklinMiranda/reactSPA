86425a677f758fe5efe38a8dc7adb74dd6fd1c81
FranklinMiranda <fdm61344@gmail.com>
2022-12-10 14:07:28 -0500

    Cm

--- reactspa/src/index.js
+++ reactspa/src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from "./App"
 import './index.css';
 
+// Uses ReactDOM.render to render the source component App to the target node root
 ReactDOM.render(<App />, document.getElementById('root'))
 
