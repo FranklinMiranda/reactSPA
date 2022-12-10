e2fb3566dc2bc0761fdb52b97797bc6384e7e3e7
FranklinMiranda <fdm61344@gmail.com>
2022-12-10 13:14:29 -0500

    Cm

---
+++ reactspa/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container0">
+                <h1>1st React Single Page Application</h1>
+            </div>
+        )
+    }
+}
