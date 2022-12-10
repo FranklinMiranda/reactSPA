2b70484883292ccb39ece9ce1db8f323de06f988
FranklinMiranda <fdm61344@gmail.com>
2022-12-10 13:18:18 -0500

    Cm

--- reactspa/src/App.js
+++ reactspa/src/App.js
@@ -3,9 +3,11 @@ import React from "react";
 class App extends React.Component {
     render() {
         return (
-            <div className="container0">
+            <div className="App">
                 <h1>1st React Single Page Application</h1>
             </div>
         )
     }
 }
+
+export default App
